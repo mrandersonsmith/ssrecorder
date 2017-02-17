10104bb728559c2c1533066dad4e9da69b842fad
mrandersonsmith <byronbb@shaw.ca>
2017-02-17 15:24:40 -0800

    Create record.sh

---
+++ record.sh
@@ -0,0 +1,26 @@
+
+#! /bin/bash
+# ./record.sh <CHANNELNUMBERXX> <HH:MM:SS or --notime>  <FILE.MP4>
+# You need to have *ffmpeg* installed AND *jq* json parser for this script to function
+# If you have issues it will probably be because the base rtmp url port number is wrong for you. Thus find out the correct 
+# port by generating an rtmp link and modifying line 20 of this script.
+# Correct usage is ./record.sh 01 1:30:00 ouput.mp4 OR ./record.sh 01 --notime ouput.mp4 which requires you to 
+# stop the recording manually. 
+# Chan number must be 2 or 3 digits. 
+# I recommend calling this script with linux command *at* https://linux.die.net/man/1/at
+
+#edit for your account.
+user=dumbfred
+pass=hasabadpassword
+
+#change site variable to reflect the service you use. 
+#Mystreams/usport=viewms Live247=view247, Starstreams=viewss, MMA-TV/Myshout=viewmma, Streamtvnow=viewstvn
+site=view247
+urlkey=$(curl "http://smoothstreams.tv/schedule/admin/dash_new/hash_api.php?username=$user&password=$pass&site=$site" | jq -r .hash) 
+url=rtmp://dna.smoothstreams.tv:3615/view247?wmsAuthSign=$urlkey/ch$1q1.stream
+
+if [[ $2 == "--notime" ]]; then
+  ffmpeg -i $url -vcodec copy -acodec copy $3
+else
+  ffmpeg -i $url -vcodec copy -acodec copy -t $2 $3 
+fi
