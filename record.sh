555aa47ffba639f34305e432a2cf83daff25e2fa
mrandersonsmith <byronbb@shaw.ca>
2020-02-07 01:31:19 -0800

    Update record.sh

--- record.sh
+++ record.sh
@@ -16,7 +16,7 @@ pass=hasabadpassword
 #change site variable to reflect the service you use. 
 #Mystreams/usport=viewms Live247=view247, Starstreams=viewss, MMA-TV/Myshout=viewmma, Streamtvnow=viewstvn
 site=view247
-urlkey=$(curl "http://auth.smoothstreams.tv/hash_api.php?site=$site&username=$user&password=$pass" | jq -r .hash)
+urlkey=$(curl "https://auth.smoothstreams.tv/hash_api.php?site=$site&username=$user&password=$pass" | jq -r .hash)
 url=rtmp://dna.smoothstreams.tv:3615/view247?wmsAuthSign=$urlkey/ch$1q1.stream
 
 if [[ $2 == "--notime" ]]; then
